b4b1d93fc3073b412bb2445ea216bba6029ed31a
kunalgrowing <growkst@gmail.com>
2024-05-29 11:25:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3 + 4)   // we are using nodejs, not browser
+
+// always remember that the code readability should be high
+
+console.log("Kunal");
+
+let name = "Kunal"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to the power 53 minus 1
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "kunal");
+console.log(typeof age);
+console.log(typeof null);   // object
+console.log(typeof undefined);    // undefined
